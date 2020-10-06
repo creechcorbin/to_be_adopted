d424434b77747d238862626ac844d7fef2343b2f
corbin creech <creechcorbin@gmail.com>
2020-10-06 14:29:49 -0400

    skeleton repo

---
+++ to_be_adopted/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for to_be_adopted project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'to_be_adopted.settings')
+
+application = get_wsgi_application()
